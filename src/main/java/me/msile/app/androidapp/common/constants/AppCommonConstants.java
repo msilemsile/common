1797a1e991654a1ba2ab0f8a889d3cc9f37737c1
mengliangliang <307104089@qq.com>
2023-06-21 07:18:41 +0800

    fix app constant

--- src/main/java/me/msile/app/androidapp/common/constants/AppCommonConstants.java
+++ src/main/java/me/msile/app/androidapp/common/constants/AppCommonConstants.java
@@ -3,7 +3,7 @@ package me.msile.app.androidapp.common.constants;
 public class AppCommonConstants {
 
     //app前缀
-    public static final String APP_PREFIX_TAG = "androidapp";
+    public static String APP_PREFIX_TAG = "androidapp";
 
-    public static final String APP_BASE_URL = "";
+    public static String APP_BASE_URL = "";
 }
