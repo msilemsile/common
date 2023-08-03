da6573eebd72e823630a163de2f6a029cba9f5c0
mengliangliang <307104089@qq.com>
2023-08-03 16:24:43 +0800

    cameraxhelper

---
+++ src/main/java/me/msile/app/androidapp/common/camera/CameraXHelper.java
@@ -0,0 +1,290 @@
+package me.msile.app.androidapp.common.camera;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.camera.core.CameraInfo;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.video.FallbackStrategy;
+import androidx.camera.video.FileOutputOptions;
+import androidx.camera.video.OutputResults;
+import androidx.camera.video.Quality;
+import androidx.camera.video.QualitySelector;
+import androidx.camera.video.Recorder;
+import androidx.camera.video.Recording;
+import androidx.camera.video.VideoCapture;
+import androidx.camera.video.VideoRecordEvent;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.core.util.Consumer;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import me.msile.app.androidapp.common.core.ActivityWeakRefHolder;
+import me.msile.app.androidapp.common.storage.StorageHelper;
+import me.msile.app.androidapp.common.ui.toast.AppToast;
+
+/**
+ * camerax帮助类
+ */
+public class CameraXHelper extends ActivityWeakRefHolder {
+
+    private static final String TAG = "CameraXHelper";
+    private ProcessCameraProvider processCameraProvider;
+    private PreviewView previewView;
+    private Preview preview;
+    private ImageCapture imageCapture;
+    private VideoCapture<Recorder> videoCapture;
+    private Recording mMediaRecorder;
+    private boolean isPrepared;
+    private boolean isCaptureStarted;
+    private boolean isFrontCamera;
+    private OnCameraHelperCallback onCameraHelperCallback;
+    private String mCameraOutputPath;
+
+    public CameraXHelper(@NonNull Activity activity) {
+        super(activity);
+    }
+
+    public void setCameraHelperCallback(OnCameraHelperCallback onCameraHelperCallback) {
+        this.onCameraHelperCallback = onCameraHelperCallback;
+    }
+
+    public void prepare(PreviewView previewView) {
+        prepare(previewView, false);
+    }
+
+    public void prepare(PreviewView previewView, boolean isFrontCamera) {
+        Activity activityWithCheck = getHolderActivityWithCheck();
+        if (activityWithCheck == null) {
+            return;
+        }
+        this.previewView = previewView;
+        this.isFrontCamera = isFrontCamera;
+        if (processCameraProvider == null) {
+            ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(activityWithCheck);
+            cameraProviderFuture.addListener(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        processCameraProvider = cameraProviderFuture.get();
+                        startPreview();
+                    } catch (Throwable e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, ContextCompat.getMainExecutor(activityWithCheck));
+        } else {
+            processCameraProvider.unbindAll();
+            startPreview();
+        }
+    }
+
+    public void startPreview() {
+        try {
+            if (processCameraProvider != null) {
+                CameraSelector cameraSelector = new CameraSelector.Builder()
+                        .requireLensFacing(isFrontCamera ? CameraSelector.LENS_FACING_FRONT : CameraSelector.LENS_FACING_BACK)
+                        .build();
+                preview = new Preview.Builder().build();
+                preview.setSurfaceProvider(previewView.getSurfaceProvider());
+                processCameraProvider.bindToLifecycle((LifecycleOwner) getHolderActivity(), cameraSelector, preview);
+            }
+            isPrepared = true;
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void switchCamera() {
+        if (!isPrepared) {
+            AppToast.toastMsg("未准备就绪");
+            return;
+        }
+        prepare(previewView, !isFrontCamera);
+    }
+
+    public void takePicture() {
+        if (!isPrepared) {
+            AppToast.toastMsg("未准备就绪");
+            return;
+        }
+        try {
+            if (processCameraProvider != null) {
+                if (imageCapture != null) {
+                    processCameraProvider.unbind(imageCapture);
+                }
+                imageCapture = new ImageCapture.Builder().build();
+                CameraSelector cameraSelector = new CameraSelector.Builder()
+                        .requireLensFacing(isFrontCamera ? CameraSelector.LENS_FACING_FRONT : CameraSelector.LENS_FACING_BACK)
+                        .build();
+                processCameraProvider.bindToLifecycle((LifecycleOwner) getHolderActivity(), cameraSelector, imageCapture);
+                mCameraOutputPath = null;
+                File cacheFile = StorageHelper.createCacheFile("takePicture_cache_" + System.currentTimeMillis() + ".jpg");
+                mCameraOutputPath = cacheFile.getAbsolutePath();
+                ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(cacheFile)
+                        .build();
+                imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(getHolderActivity()), new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        if (onCameraHelperCallback != null) {
+                            onCameraHelperCallback.onTakePictureSuccess(mCameraOutputPath);
+                        }
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        if (onCameraHelperCallback != null) {
+                            onCameraHelperCallback.onTakePictureError(exception.getMessage());
+                        }
+                    }
+                });
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopCaptureVideo() {
+        if (!isPrepared) {
+            AppToast.toastMsg("未准备就绪");
+            return;
+        }
+        if (!isCaptureStarted) {
+            AppToast.toastMsg("拍摄未开始");
+            return;
+        }
+        try {
+            if (mMediaRecorder != null) {
+                mMediaRecorder.stop();
+                mMediaRecorder = null;
+                isCaptureStarted = false;
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isCaptureStarted() {
+        return isCaptureStarted;
+    }
+
+    @Override
+    public void onClear() {
+
+    }
+
+    @SuppressLint("MissingPermission")
+    public void startCaptureVideo() {
+        if (!isPrepared) {
+            AppToast.toastMsg("未准备就绪");
+            return;
+        }
+        if (isCaptureStarted) {
+            AppToast.toastMsg("正在录制中");
+            return;
+        }
+        try {
+            if (processCameraProvider != null) {
+                if (videoCapture != null) {
+                    processCameraProvider.unbind(videoCapture);
+                }
+                CameraSelector cameraSelector = new CameraSelector.Builder()
+                        .requireLensFacing(isFrontCamera ? CameraSelector.LENS_FACING_FRONT : CameraSelector.LENS_FACING_BACK)
+                        .build();
+                List<CameraInfo> cameraInfoList = cameraSelector.filter(processCameraProvider.getAvailableCameraInfos());
+                QualitySelector qualitySelector = null;
+                if (!cameraInfoList.isEmpty()) {
+                    qualitySelector = QualitySelector.fromOrderedList(QualitySelector.getSupportedQualities(cameraInfoList.get(0)), FallbackStrategy.lowerQualityOrHigherThan(Quality.SD));
+                } else {
+                    List<Quality> qualityList = new ArrayList<>();
+                    qualityList.add(Quality.UHD);
+                    qualityList.add(Quality.FHD);
+                    qualityList.add(Quality.HD);
+                    qualityList.add(Quality.SD);
+                    qualitySelector = QualitySelector.fromOrderedList(qualityList,
+                            FallbackStrategy.lowerQualityOrHigherThan(Quality.SD));
+                }
+                Recorder recorder = new Recorder.Builder()
+                        .setQualitySelector(qualitySelector)
+                        .build();
+                videoCapture = VideoCapture.withOutput(recorder);
+                processCameraProvider.bindToLifecycle((LifecycleOwner) getHolderActivity(), cameraSelector, videoCapture);
+                mCameraOutputPath = null;
+                //output cache file
+                File captureVideoFile = StorageHelper.createCacheFile("captureVideo_cache_" + System.currentTimeMillis() + ".mp4");
+                mCameraOutputPath = captureVideoFile.getAbsolutePath();
+                FileOutputOptions fileOutputOptions = new FileOutputOptions.Builder(captureVideoFile).build();
+                mMediaRecorder = recorder.prepareRecording(getHolderActivity(), fileOutputOptions)
+                        .withAudioEnabled()
+                        .start(ContextCompat.getMainExecutor(getHolderActivity()), new Consumer<VideoRecordEvent>() {
+                            @Override
+                            public void accept(VideoRecordEvent videoRecordEvent) {
+                                if (videoRecordEvent instanceof VideoRecordEvent.Start) {
+                                    if (onCameraHelperCallback != null) {
+                                        onCameraHelperCallback.onCaptureVideoStart();
+                                    }
+                                    isCaptureStarted = true;
+                                } else if (videoRecordEvent instanceof VideoRecordEvent.Pause) {
+
+                                } else if (videoRecordEvent instanceof VideoRecordEvent.Resume) {
+
+                                } else if (videoRecordEvent instanceof VideoRecordEvent.Finalize) {
+                                    VideoRecordEvent.Finalize finalizeEvent = (VideoRecordEvent.Finalize) videoRecordEvent;              // Handles a finalize event for the active recording, checking Finalize.getError()
+                                    int error = finalizeEvent.getError();
+                                    if (error != VideoRecordEvent.Finalize.ERROR_NONE) {
+                                        if (onCameraHelperCallback != null) {
+                                            if (finalizeEvent.hasError()) {
+                                                onCameraHelperCallback.onCaptureVideoError(finalizeEvent.getCause().getMessage());
+                                            } else {
+                                                onCameraHelperCallback.onCaptureVideoError("finalizeEvent error: " + error);
+                                            }
+                                        }
+                                    } else {
+                                        OutputResults outputResults = finalizeEvent.getOutputResults();
+                                        Log.i(TAG, "VideoRecordEvent.Finalize output : " + outputResults.getOutputUri());
+                                        if (onCameraHelperCallback != null) {
+                                            onCameraHelperCallback.onCaptureVideoSuccess(mCameraOutputPath);
+                                        }
+                                    }
+                                }
+                            }
+                        });
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+            if (onCameraHelperCallback != null) {
+                onCameraHelperCallback.onCaptureVideoError(e.getMessage());
+            }
+        }
+    }
+
+    public interface OnCameraHelperCallback {
+        default void onTakePictureSuccess(String picPath) {
+        }
+
+        default void onTakePictureError(String message) {
+        }
+
+        default void onCaptureVideoSuccess(String videoPath) {
+        }
+
+        default void onCaptureVideoError(String message) {
+        }
+
+        default void onCaptureVideoStart() {
+        }
+    }
+}
---
+++ src/main/java/me/msile/app/androidapp/common/camera/SimpleCameraXActivity.java
@@ -0,0 +1,178 @@
+package me.msile.app.androidapp.common.camera;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.camera.view.PreviewView;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+import me.msile.app.androidapp.common.R;
+import me.msile.app.androidapp.common.glide.GlideApp;
+import me.msile.app.androidapp.common.permissions.PermissionHelper;
+import me.msile.app.androidapp.common.permissions.callback.PermissionCallback;
+import me.msile.app.androidapp.common.rx.AutoDisposeUtils;
+import me.msile.app.androidapp.common.rx.DefaultDisposeObserver;
+import me.msile.app.androidapp.common.storage.MediaInsertHelper;
+import me.msile.app.androidapp.common.ui.activity.ImmerseFullScreenActivity;
+import me.msile.app.androidapp.common.ui.dialog.AppAlertDialog;
+import me.msile.app.androidapp.common.ui.widget.shapelayout.ShapeImageView;
+
+public class SimpleCameraXActivity extends ImmerseFullScreenActivity {
+
+    private PreviewView surfaceView;
+    private ShapeImageView ivCenter;
+    private TextView tvCaptureStatus;
+    private ImageView ivCamera;
+    private ImageView ivClose;
+    private ImageView ivSwitch;
+    private CameraXHelper cameraHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_camerax);
+        initView();
+        PermissionHelper.with(this)
+                .requestPermission(new SimpleCameraPermission(), new PermissionCallback() {
+                    @Override
+                    public void onGranted() {
+                        initData();
+                    }
+
+                    @Override
+                    public void onCancel() {
+
+                    }
+                });
+    }
+
+    private void initView() {
+        surfaceView = (PreviewView) findViewById(R.id.surface_view);
+        tvCaptureStatus = (TextView) findViewById(R.id.tv_capture_status);
+        ivClose = (ImageView) findViewById(R.id.iv_close);
+        ivSwitch = (ImageView) findViewById(R.id.iv_switch);
+        ivClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        ivSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraHelper.switchCamera();
+            }
+        });
+        ivCamera = (ImageView) findViewById(R.id.iv_camera);
+        ivCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cameraHelper.isCaptureStarted()) {
+                    cameraHelper.stopCaptureVideo();
+                } else {
+                    cameraHelper.takePicture();
+                }
+            }
+        });
+        ivCamera.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                cameraHelper.startCaptureVideo();
+                return true;
+            }
+        });
+        ivCenter = (ShapeImageView) findViewById(R.id.iv_center);
+        ivCenter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ivCenter.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void initData() {
+        cameraHelper = getWeakRefHolder(CameraXHelper.class);
+        cameraHelper.prepare(surfaceView);
+        cameraHelper.setCameraHelperCallback(new CameraXHelper.OnCameraHelperCallback() {
+            @Override
+            public void onTakePictureSuccess(String picPath) {
+                ivCenter.setVisibility(View.VISIBLE);
+                GlideApp.with(SimpleCameraXActivity.this).load(picPath).into(ivCenter);
+                MediaInsertHelper.insertPicToGallery(picPath);
+            }
+
+            @Override
+            public void onTakePictureError(String message) {
+                AppAlertDialog.build().setTitleText("拍照失败")
+                        .setContentText(message)
+                        .setConfirmText("确定")
+                        .show(SimpleCameraXActivity.this);
+            }
+
+            @Override
+            public void onCaptureVideoSuccess(String videoPath) {
+                refreshStopCaptureStatus();
+                ivCenter.setVisibility(View.VISIBLE);
+                GlideApp.with(SimpleCameraXActivity.this).load(videoPath).into(ivCenter);
+                MediaInsertHelper.insertVideoToMedia(videoPath);
+            }
+
+            @Override
+            public void onCaptureVideoError(String message) {
+                refreshStopCaptureStatus();
+                AppAlertDialog.build().setTitleText("录像失败")
+                        .setContentText(message)
+                        .setConfirmText("确定")
+                        .show(SimpleCameraXActivity.this);
+            }
+
+            @Override
+            public void onCaptureVideoStart() {
+                refreshStartCaptureStatus();
+            }
+        });
+    }
+
+    private DefaultDisposeObserver<Long> captureStatusObserver;
+    private long captureTime;
+
+    private void refreshStartCaptureStatus() {
+        refreshStopCaptureStatus();
+        tvCaptureStatus.setText("正在录制");
+        ivCamera.setImageResource(R.mipmap.icon_record_press);
+        captureTime = 0;
+        captureStatusObserver = new DefaultDisposeObserver<Long>() {
+            @Override
+            protected void onSuccess(Long aLong) {
+                captureTime++;
+                tvCaptureStatus.setText("正在录制: " + captureTime + "秒");
+            }
+        };
+        Observable.interval(1, 1, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .to(AutoDisposeUtils.onDestroyDispose(this))
+                .subscribe(captureStatusObserver);
+    }
+
+    private void refreshStopCaptureStatus() {
+        if (captureStatusObserver != null) {
+            captureStatusObserver.dispose();
+            captureStatusObserver = null;
+        }
+        tvCaptureStatus.setText("点击拍照,长按录像");
+        ivCamera.setImageResource(R.mipmap.icon_record_normal);
+    }
+
+    public static void goToPage(Context context) {
+        Intent intent = new Intent(context, SimpleCameraXActivity.class);
+        context.startActivity(intent);
+    }
+}
