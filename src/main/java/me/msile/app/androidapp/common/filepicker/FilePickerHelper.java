2c0562eb94dc5b900c3c79dd37d5ebc749a25967
mengliangliang <307104089@qq.com>
2023-08-07 15:09:06 +0800

    filepickhelper

---
+++ src/main/java/me/msile/app/androidapp/common/filepicker/FilePickerHelper.java
@@ -0,0 +1,259 @@
+package me.msile.app.androidapp.common.filepicker;
+
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+
+import java.io.File;
+
+import me.msile.app.androidapp.common.core.ActivityMethodProxy;
+import me.msile.app.androidapp.common.core.ActivityWeakRefHolder;
+import me.msile.app.androidapp.common.permissions.PermissionHelper;
+import me.msile.app.androidapp.common.permissions.callback.PermissionCallback;
+import me.msile.app.androidapp.common.permissions.request.CameraPermissionRequest;
+import me.msile.app.androidapp.common.provider.FileProviderHelper;
+import me.msile.app.androidapp.common.storage.StorageHelper;
+import me.msile.app.androidapp.common.ui.activity.BaseActivity;
+import me.msile.app.androidapp.common.ui.toast.AppToast;
+
+/**
+ * 选择文件弹窗（文件、相册、拍照、录像）
+ */
+public class FilePickerHelper extends ActivityWeakRefHolder implements ActivityMethodProxy {
+
+    public static final int PICK_TYPE_FILE = 1;
+    public static final int PICK_TYPE_GALLERY = 2;
+    public static final int PICK_TYPE_IMAGE_CAPTURE = 3;
+    public static final int PICK_TYPE_VIDEO_CAPTURE = 4;
+    public static final int PICK_TYPE_GALLERY_MULTI = 5;
+
+    private OnPickFileListener pickFileListener;
+
+    public FilePickerHelper(@NonNull Activity activity) {
+        super(activity);
+        if (activity instanceof BaseActivity) {
+            BaseActivity baseActivity = (BaseActivity) activity;
+            baseActivity.addActivityMethodCallback(this);
+        }
+    }
+
+    public void setAppPickFileListener(OnPickFileListener onPickFileListener) {
+        this.pickFileListener = onPickFileListener;
+    }
+
+    public static FilePickerHelper with(Activity activity) {
+        return new FilePickerHelper(activity);
+    }
+
+    /**
+     * 选择文件
+     */
+    public void startPickFile() {
+        try {
+            Intent pickFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            pickFileIntent.addCategory(Intent.CATEGORY_OPENABLE);
+            pickFileIntent.setType("*/*");
+            pickFileIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            getHolderActivity().startActivityForResult(pickFileIntent, PICK_TYPE_FILE);
+        } catch (Exception e) {
+            AppToast.toastMsg("选择文件失败");
+            Log.d("AppPickDialog", "startPickFile error");
+        }
+    }
+
+    /**
+     * 相册(多选)
+     */
+    public void startPickFromGalleryMulti() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            try {
+                Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
+                int pickImagesMaxLimit = MediaStore.getPickImagesMaxLimit();
+                intent.putExtra(MediaStore.EXTRA_PICK_IMAGES_MAX, pickImagesMaxLimit);
+                getHolderActivity().startActivityForResult(intent, PICK_TYPE_GALLERY_MULTI);
+            } catch (Exception e) {
+                AppToast.toastMsg("打开相册失败");
+                Log.d("AppPickDialog", "startPickFromGallery error");
+            }
+        } else {
+            AppToast.toastMsg("系统版本太低(>=13)，不支持相册多选");
+        }
+    }
+
+    /**
+     * 相册
+     */
+    public void startPickFromGallery() {
+        try {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+            getHolderActivity().startActivityForResult(intent, PICK_TYPE_GALLERY);
+        } catch (Exception e) {
+            AppToast.toastMsg("打开相册失败");
+            Log.d("AppPickDialog", "startPickFromGallery error");
+        }
+    }
+
+    //处理拍照和录像 返回data可能null
+    private Uri cachePickFileUri = null;
+    private String cachePickFilePath = null;
+
+    /**
+     * 拍照
+     */
+    public void startImageCapture() {
+        PermissionHelper.with((FragmentActivity) getHolderActivity()).requestPermission(new CameraPermissionRequest(), new PermissionCallback() {
+            @Override
+            public void onGranted() {
+                try {
+                    Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    String fileName = "image_capture_" + System.currentTimeMillis() + ".jpg";
+                    File cacheImageFile = StorageHelper.createShareFile(fileName);
+                    Uri uri = FileProviderHelper.fromFile(cacheImageFile);
+                    cachePickFileUri = uri;
+                    cachePickFilePath = cacheImageFile.getAbsolutePath();
+                    captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                    getHolderActivity().startActivityForResult(captureIntent, PICK_TYPE_IMAGE_CAPTURE);
+                } catch (Exception e) {
+                    AppToast.toastMsg("打开相机失败");
+                    Log.d("AppPickDialog", "onClickImageCapture error");
+                }
+            }
+
+            @Override
+            public void onCancel() {
+                if (pickFileListener != null) {
+                    pickFileListener.onPickCancel();
+                }
+            }
+
+            @Override
+            public void onGoSetting() {
+
+            }
+        });
+    }
+
+    /**
+     * 录像
+     */
+    public void startVideoRecord() {
+        PermissionHelper.with((FragmentActivity) getHolderActivity()).requestPermission(new CameraPermissionRequest(), new PermissionCallback() {
+            @Override
+            public void onGranted() {
+                try {
+                    Intent captureIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                    String fileName = "video_record_" + System.currentTimeMillis() + ".mp4";
+                    File cacheVideoFile = StorageHelper.createShareFile(fileName);
+                    Uri uri = FileProviderHelper.fromFile(cacheVideoFile);
+                    cachePickFileUri = uri;
+                    cachePickFilePath = cacheVideoFile.getAbsolutePath();
+                    captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                    getHolderActivity().startActivityForResult(captureIntent, PICK_TYPE_VIDEO_CAPTURE);
+                } catch (Exception e) {
+                    AppToast.toastMsg("打开录像失败");
+                    Log.d("AppPickDialog", "onClickImageCapture error");
+                }
+            }
+
+            @Override
+            public void onCancel() {
+                if (pickFileListener != null) {
+                    pickFileListener.onPickCancel();
+                }
+            }
+
+            @Override
+            public void onGoSetting() {
+
+            }
+        });
+    }
+
+    /**
+     * 处理选择文件/多选相册回调
+     */
+    private void handlePickFileResult(Intent intent) {
+        Uri[] results = null;
+        if (intent != null) {
+            String dataString = intent.getDataString();
+            ClipData clipData = intent.getClipData();
+            if (clipData != null) {
+                int clipDataCount = clipData.getItemCount();
+                if (clipDataCount > 0) {
+                    results = new Uri[clipDataCount];
+                    for (int i = 0; i < clipDataCount; i++) {
+                        ClipData.Item item = clipData.getItemAt(i);
+                        results[i] = item.getUri();
+                    }
+                }
+            }
+            if (dataString != null) {
+                results = new Uri[]{intent.getData()};
+            }
+        }
+        if (pickFileListener != null) {
+            pickFileListener.onPickFile(results);
+        }
+    }
+
+    /**
+     * 处理媒体文件
+     */
+    private void handlePickMediaFileResult(Intent data) {
+        Uri result = null;
+        if (data != null) {
+            result = data.getData();
+        }
+        if (result == null) {
+            if (cachePickFileUri != null) {
+                result = cachePickFileUri;
+            }
+        }
+        if (pickFileListener != null) {
+            pickFileListener.onPickFile(new Uri[]{result});
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case PICK_TYPE_FILE:
+                case PICK_TYPE_GALLERY_MULTI:
+                    handlePickFileResult(data);
+                    break;
+                case PICK_TYPE_GALLERY:
+                case PICK_TYPE_IMAGE_CAPTURE:
+                case PICK_TYPE_VIDEO_CAPTURE:
+                    handlePickMediaFileResult(data);
+                    break;
+            }
+        } else {
+            if (pickFileListener != null) {
+                pickFileListener.onPickCancel();
+            }
+        }
+    }
+
+    @Override
+    public void onClear() {
+
+    }
+
+    public interface OnPickFileListener {
+        default void onPickFile(@Nullable Uri[] uri) {
+        }
+
+        default void onPickCancel() {
+        }
+    }
+}
---
+++ src/main/java/me/msile/app/androidapp/common/filepicker/SimpleFilePickerDialog.java
@@ -0,0 +1,220 @@
+package me.msile.app.androidapp.common.filepicker;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import me.msile.app.androidapp.common.R;
+import me.msile.app.androidapp.common.ui.dialog.BaseRecyclerDialog;
+
+/**
+ * 选择文件弹窗（文件、相册、拍照、录像）
+ */
+public class SimpleFilePickerDialog extends BaseRecyclerDialog {
+
+    private LinearLayout llPickContent;
+    private FilePickerHelper.OnPickFileListener pickFileListener;
+
+    private boolean allowPickMultiFile;
+    private boolean showGallery;
+    private boolean showGalleryMulti;
+    private boolean showFilePick;
+    private boolean showImageCapture;
+    private boolean showVideoCapture;
+    private boolean showAllPickOperate;
+    private int autoStartPickType;
+    private int currentPickType;
+    private FilePickerHelper mFilePickerHelper;
+
+    @Override
+    protected int getLayoutResId() {
+        return me.msile.app.androidapp.common.R.layout.dialog_app_pick_file;
+    }
+
+    @Override
+    protected void initViews(View rootView) {
+        llPickContent = rootView.findViewById(R.id.ll_pick_content);
+        llPickContent.removeAllViews();
+    }
+
+    @Override
+    protected void initData(boolean isFirstInit, @Nullable Bundle savedInstanceState) {
+        mFilePickerHelper = new FilePickerHelper(mActivity);
+        mFilePickerHelper.setAppPickFileListener(pickFileListener);
+        if (savedInstanceState != null) {
+            allowPickMultiFile = savedInstanceState.getBoolean("allowPickMultiFile");
+            showGallery = savedInstanceState.getBoolean("showGallery");
+            showGalleryMulti = savedInstanceState.getBoolean("showGalleryMulti");
+            showFilePick = savedInstanceState.getBoolean("showFilePick");
+            showImageCapture = savedInstanceState.getBoolean("showImageCapture");
+            showVideoCapture = savedInstanceState.getBoolean("showVideoCapture");
+            showAllPickOperate = savedInstanceState.getBoolean("showAllPickOperate");
+            autoStartPickType = savedInstanceState.getInt("autoStartPickType");
+            currentPickType = savedInstanceState.getInt("currentPickType");
+            Log.d("AppPickDialog", "initData restore savedInstanceState");
+        }
+        if (autoStartPickType != 0) {
+            onClickPickFileItem(autoStartPickType);
+        } else {
+            initPickItems();
+            //最后一个item 不展示底部分割线
+            int childCount = llPickContent.getChildCount();
+            if (childCount > 0) {
+                View childAt = llPickContent.getChildAt(childCount - 1);
+                View divider = childAt.findViewById(R.id.bottom_divider);
+                if (divider != null) {
+                    divider.setVisibility(View.GONE);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean("allowPickMultiFile", allowPickMultiFile);
+        outState.putBoolean("showGallery", showGallery);
+        outState.putBoolean("showFilePick", showFilePick);
+        outState.putBoolean("showImageCapture", showImageCapture);
+        outState.putBoolean("showVideoCapture", showVideoCapture);
+        outState.putBoolean("showAllPickOperate", showAllPickOperate);
+        outState.putInt("autoStartPickType", autoStartPickType);
+        outState.putInt("currentPickType", currentPickType);
+        Log.d("AppPickDialog", "onSaveInstanceState");
+    }
+
+    private void initPickItems() {
+        //拍照
+        if (showImageCapture || showAllPickOperate) {
+            buildPickFileItem(FilePickerHelper.PICK_TYPE_IMAGE_CAPTURE);
+        }
+        //录像
+        if (showVideoCapture || showAllPickOperate) {
+            buildPickFileItem(FilePickerHelper.PICK_TYPE_VIDEO_CAPTURE);
+        }
+        //相册
+        if (showGallery || showAllPickOperate) {
+            buildPickFileItem(FilePickerHelper.PICK_TYPE_GALLERY);
+        }
+        //文件
+        if (showFilePick || showAllPickOperate) {
+            buildPickFileItem(FilePickerHelper.PICK_TYPE_FILE);
+        }
+        //相册多选
+        if (showGalleryMulti || showAllPickOperate) {
+            buildPickFileItem(FilePickerHelper.PICK_TYPE_GALLERY_MULTI);
+        }
+        //取消
+        buildPickFileItem(0);
+    }
+
+    public SimpleFilePickerDialog setShowAllPickOperate(boolean showAllPickOperate) {
+        this.showAllPickOperate = showAllPickOperate;
+        return this;
+    }
+
+    public SimpleFilePickerDialog setAllowPickMultiFile(boolean allowPickMultiFile) {
+        this.allowPickMultiFile = allowPickMultiFile;
+        return this;
+    }
+
+    public SimpleFilePickerDialog setShowFilePick(boolean showFilePick) {
+        this.showFilePick = showFilePick;
+        return this;
+    }
+
+    public SimpleFilePickerDialog setShowGallery(boolean showGallery) {
+        this.showGallery = showGallery;
+        return this;
+    }
+
+    public SimpleFilePickerDialog setShowImageCapture(boolean showImageCapture) {
+        this.showImageCapture = showImageCapture;
+        return this;
+    }
+
+    public SimpleFilePickerDialog setShowVideoCapture(boolean showVideoCapture) {
+        this.showVideoCapture = showVideoCapture;
+        return this;
+    }
+
+    public SimpleFilePickerDialog setAutoStartPickType(int autoStartPickType) {
+        this.autoStartPickType = autoStartPickType;
+        return this;
+    }
+
+    public SimpleFilePickerDialog setPickFileListener(FilePickerHelper.OnPickFileListener appPickFileListener) {
+        this.pickFileListener = appPickFileListener;
+        return this;
+    }
+
+    private void buildPickFileItem(int pickType) {
+        View itemView = LayoutInflater.from(mActivity).inflate(R.layout.item_app_pick_file, llPickContent, false);
+        TextView tvPick = itemView.findViewById(R.id.tv_pick);
+        switch (pickType) {
+            case FilePickerHelper.PICK_TYPE_FILE:
+                tvPick.setText(allowPickMultiFile ? "文件(可多选)" : "文件");
+                break;
+            case FilePickerHelper.PICK_TYPE_GALLERY:
+                tvPick.setText("相册");
+                break;
+            case FilePickerHelper.PICK_TYPE_IMAGE_CAPTURE:
+                tvPick.setText("拍照");
+                break;
+            case FilePickerHelper.PICK_TYPE_VIDEO_CAPTURE:
+                tvPick.setText("录像");
+                break;
+            case FilePickerHelper.PICK_TYPE_GALLERY_MULTI:
+                tvPick.setText("相册(可多选)");
+                break;
+            case 0:
+                tvPick.setText("取消");
+                tvPick.setTextColor(0xff999999);
+                break;
+        }
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickPickFileItem(pickType);
+            }
+        });
+        llPickContent.addView(itemView);
+    }
+
+    private void onClickPickFileItem(int pickType) {
+        switch (pickType) {
+            case FilePickerHelper.PICK_TYPE_FILE:
+                mFilePickerHelper.startPickFile();
+                break;
+            case FilePickerHelper.PICK_TYPE_GALLERY:
+                mFilePickerHelper.startPickFromGallery();
+                break;
+            case FilePickerHelper.PICK_TYPE_IMAGE_CAPTURE:
+                mFilePickerHelper.startImageCapture();
+                break;
+            case FilePickerHelper.PICK_TYPE_VIDEO_CAPTURE:
+                mFilePickerHelper.startVideoRecord();
+                break;
+            case FilePickerHelper.PICK_TYPE_GALLERY_MULTI:
+                mFilePickerHelper.startPickFromGalleryMulti();
+                break;
+            case 0:
+                if (pickFileListener != null) {
+                    pickFileListener.onPickCancel();
+                }
+                dismiss();
+                break;
+        }
+        currentPickType = pickType;
+    }
+
+    public static SimpleFilePickerDialog build() {
+        return new SimpleFilePickerDialog();
+    }
+}
